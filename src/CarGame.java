6d9e4fec38d0bc9000c9b4f5d85be3d6a611d3dd
Jenis Bhattarai <bjenish49@gmail.com>
2024-07-15 11:28:44 +0545

    Initial commit

---
+++ src/CarGame.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class CarGame {
+    public static void main(String[] args) {
+        String i;
+        while( i = "Quit"){
+
+        }
+        }
+    }
